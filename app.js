9fbc4dca02d78df4158d0a172c526b49ab2f4944
uranoni <uranoni777@gmail.com>
2018-11-25 23:33:26 +0800

    add explain

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 const mongoose = require('mongoose');
 var cors = require('cors')
-
-
+var app= express();
+app.use(express.static(__dirname+'/www'));
 mongoose.Promise = global.Promise;
+// 資料庫的連線斜線後面是資料庫名稱
 mongoose.connect('mongodb://localhost:27017/noteapp', {
     // useMongoClient: true
 });
 
-
 var app = express();
 app.use(cors())
 app.use(express.static(__dirname + '/www'));
+//bodyParser 前端傳過來的資料要做解析
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+
+//
 var Schema = new mongoose.Schema({ user: 'string', context: 'string' });
 var Post = mongoose.model('Post', Schema);
 
-app.post('/save', (req, res) => {
-    var body = _.pick(req.body, ['context', 'user'])
+// app.post('/save', (req, res) => {
+//     var body = _.pick(req.body, ['context', 'user'])
 
-    const Opost = new Post({ user: body.user, context: body.context });
-    Opost.save().then(() => console.log('meow'));
-    res.send("ok")
-})
+//     const Opost = new Post({ user: body.user, context: body.context });
+//     Opost.save().then(() => console.log('meow'));
+//     res.send("ok")
+// })
 
 // app.post('/rewrite', (req, res) => {
 //     var body = _.pick(req.body, ['context', 'user'])
@@ -41,6 +44,8 @@ app.post('/save', (req, res) => {
 
 app.post('/rewrite', (req, res) => {
     var body = _.pick(req.body, ['context', 'user'])
+    // 第一個參數你要找的是誰
+    // 第二個參數 你要改變的值 用set
     Post.findOneAndUpdate({ user: 'user' }, { $set: { context: body.context } },{ new: true }, function(error, result) {
         if (!error) {
             // If the document doesn't exist
@@ -52,17 +57,16 @@ app.post('/rewrite', (req, res) => {
             // Save the document
             result.save(function(error) {
                 if (!error) {
-                    // Do something with the document
+                  res.send(result);
                 } else {
-                    throw error;
+                  res.send(error)
                 }
             });
         }
     });
+    
 })
 
-
-app.listen(8080, () => {
-    // console.log(moment().tz("Asia/Taipei").format());
-    console.log("http://localhost:8080")
+app.listen(8787,()=>{
+  console.log("http://localhost:8787")
 })
--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,35 @@
+// 新增vue的實例
 new Vue({
+  // 新增要控制誰用id來操作
   el: '#app',
+  // 資料
   data: {
+    //起始值
     input: '# hello',
     tmp: " "
   },
+  // 計算功能
   computed: {
+    // 轉譯功能的function 吐回到data.tmp
     compiledMarkdown: function () {
       tmp = marked(this.input, { sanitize: true })
       return tmp
     }
   },
+  // 方法作用控制function的事件(點擊或是輸入更新)
   methods: {
-
+    //即時更新把左邊的markdown語法轉換到右邊
     update:
       _.debounce(function (e) {
         this.input = e.target.value
       }, 300),
+    // 把資料傳到後端
     test: function () {
+      // 傳送資料到後端的方法
+      // post 後面接url  第二個參數接資料
+      
       axios
-        .post('http://localhost:8080/rewrite', { user: 'user', context: tmp })
+        .post('http://localhost:8787/rewrite', { user: 'user', context: tmp })
         .then(res => { console.log(res) })
         .catch(err => { console.log(err) })
     }
